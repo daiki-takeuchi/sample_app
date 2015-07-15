e5d3ef67c06707b397b73f3466aac56dea696996
d.takeuchi <d.takeuchi@technoplan-inc.co.jp>
2015-07-16 01:40:46 +0900

    第11章 ユーザーをフォローする
    11.1.2User/relationshipの関連付け

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,9 @@ RSpec.describe User, type: :model do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
 
+  # it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+
   it { should be_valid }
   it { should_not be_admin }
 

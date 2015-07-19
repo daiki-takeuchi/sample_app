d0042bcb616f53ada4ff40f3cd57b695c67d6e8e
d.takeuchi <d.takeuchi@technoplan-inc.co.jp>
2015-07-19 15:03:20 +0900

    第11章 ユーザーをフォローする
    完了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -125,6 +125,12 @@ RSpec.describe User, type: :model do
 
   describe "remember token" do
     before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
     its(:remember_token) { should_not be_blank }
   end
 
@@ -155,10 +161,21 @@ RSpec.describe User, type: :model do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 
